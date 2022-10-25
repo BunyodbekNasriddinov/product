082ea49f32635ce7912d5678d39151b84bd08bde
jovliyevquvonch <jovliyevquvonchbek93@gmail.com>
2022-10-25 21:45:29 +0500

    footer section style change

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ const elDarkBtn = document.querySelector(".mode");
 const elLoginbtn = document.querySelector(".login");
 const elLogo = document.querySelector(".logo");
 const elImg = document.querySelector(".logo-link__pic");
+const productSection = document.querySelector(".product");
+const processSection = document.querySelector(".process");
+// const elProduct = document.querySelector(".product");
 
 elDarkBtn.addEventListener("click", () => {
   elBody.classList.toggle("dark");
   elLogo.classList.toggle("logo");
+  productSection.classList.toggle("product-dark");
+  processSection.classList.toggle("process-dark")
+  processSection.classList.add("process-dark");
   elImg.style.display = "none";
 });
+
+// console.log(productSection);
