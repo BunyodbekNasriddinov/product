8f7b84467bc774e2ba31b5cf72fbc8a46b37ace3
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2022-11-01 14:06:18 +0500

    main.js modified

--- js/main.js
+++ js/main.js
@@ -2,15 +2,14 @@ var elBody = document.querySelector("body");
 var elDarkBtn = document.querySelector(".mode-btn");
 var elLogo = document.querySelector(".logo");
 var elImg = document.querySelector(".logo__img");
-var elFooterImg = document.querySelector(".logo-link__pic");
-var productSection = document.querySelector(".product");
-var processSection = document.querySelector(".process");
 
 elDarkBtn.addEventListener("click", () => {
   elBody.classList.toggle("dark");
 
   if (elBody.classList[0] === 'dark') {
     elImg.src = "../images/site-logo-mode.svg";
+  } else {
+    elImg.src = "../images/logo.svg"; 
   }
 });
 
