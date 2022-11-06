d731ca6af89dde57b53bb81bfc1ac8716a10c16b
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2022-11-06 23:07:38 +0500

    main.js logo src attribute path fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 var elBody = document.querySelector("body");
 var elDarkBtn = document.querySelector(".mode-btn");
 var elLogo = document.querySelector(".logo");
-var elImg = document.querySelector(".logo__img");
+var elImg = document.querySelectorAll(".logo__img");
 
 elDarkBtn.addEventListener("click", () => {
   elBody.classList.toggle("dark");
 
   if (elBody.classList[0] === 'dark') {
-    elImg.src = "../images/site-logo-mode.svg";
+    elImg[0].src = "../images/site-logo-mode.svg";
+    elImg[1].src = "../images/site-logo-mode.svg";
   } else {
-    elImg.src = "../images/logo.svg"; 
+    elImg[0].src = "../images/logo.svg";
+    elImg[1].src = "../images/logo.svg";
   }
 });
 
-// console.log();
+// console.log(elImg);
