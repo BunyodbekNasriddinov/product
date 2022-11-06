08e973124c65084fac0dfe076ab74a5de5b04780
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2022-11-07 04:28:37 +0500

    customers and resources pages added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var elBody = document.querySelector("body");
 var elDarkBtn = document.querySelector(".mode-btn");
 var elLogo = document.querySelector(".logo");
 var elImg = document.querySelectorAll(".logo__img");
+var menuBtn = document.querySelector('.menu-btn');
+var elNavbar = document.querySelector('.navbar');
+var elNavbarList = document.querySelector('.navbar__list');
 
 elDarkBtn.addEventListener("click", () => {
   elBody.classList.toggle("dark");
@@ -14,5 +17,9 @@ elDarkBtn.addEventListener("click", () => {
     elImg[1].src = "../images/logo.svg";
   }
 });
+
+menuBtn.addEventListener('click', () => {
+  elNavbar.style.display = 'block';
+})
 
 // console.log(elImg);
